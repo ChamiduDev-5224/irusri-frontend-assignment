fb94c1a19a9693021b5e1fe780228f9a3b987c11
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2024-12-02 11:32:53 +0530

    url file added

---
+++ src/utility/Url.js
@@ -0,0 +1,110 @@
+export const urls = {
+    arrivalInfo: [
+      {
+        url: "/icons/new-arrivals/ar1.svg",
+        alt: "Arrival one",
+        name:"T-SHIRT WITH TAPE DETAILS",
+        price:"2,500LKR",
+        isDiscount:false,
+        oldPrice:"2,600LKR",
+        discount:"-5%"
+      },
+      {
+        url: "/icons/new-arrivals/ar2.svg",
+        alt: "Arrival Two",
+        name:'SKINNY FIT JEANS',
+        price:"6,500LKR",
+        isDiscount:true,
+        oldPrice:"7,600LKR",
+        discount:"-20%"
+  
+      },
+      {
+        url: "/icons/new-arrivals/ar3.svg",
+        alt: "Arrival Three",
+        name:'CHECKERED SHIRT',
+        price:"1,000LKR",
+        isDiscount:true,
+        oldPrice:"1,400LKR",
+        discount:"-12%"
+  
+      },
+      {
+        url: "/icons/new-arrivals/ar4.svg",
+        alt: "Arrival Four",
+        name:'SLEEVE STRIPED T-SHIRT',
+        price:"4,000LKR",
+        isDiscount:false,
+        oldPrice:"5,400LKR",
+        discount:"-26%"
+  
+      },
+    ],
+    topSaleInfo:[{
+      url:"/icons/top-selling/tp-1.svg",
+      alt:"Top Sale one",
+      name:'VERTICAL STRIPED SHIRT',
+      price:"3,450LKR",
+      isDiscount:true,
+      oldPrice:"4,000LKR",
+      discount:"-16%"
+    },{
+      url:"/icons/top-selling/tp-2.svg",
+      alt:"Top Sale two",
+      name:'FADED SKINNY JEANS',
+      price:"7,530LKR",
+      isDiscount:false,
+      oldPrice:"8,600LKR",
+      discount:"-12%"
+      
+    },{
+      url:"/icons/top-selling/tp-3.svg",
+      alt:"Top Sale three",
+      name:'COURAGE GRAPHIC T-SHIRT',
+      price:"2,250LKR",
+      isDiscount:true,
+      oldPrice:"3,400LKR",
+      discount:"-25%"
+    },{
+      url:"/icons/top-selling/tp-4.svg",
+      alt:"Top Sale four",
+      name:'LOOSE FIT BERMUDA SHORTS',
+      price:"4,678LKR",
+      isDiscount:false,
+      oldPrice:"5,000LKR",
+      discount:"-11%"
+    }],
+    browseDress:[{
+      url:"/icons/browse-by/bb1.svg",
+      alt:"browse 1",
+    },{
+      url:"/icons/browse-by/bb2.svg",
+      alt:"browse 2",
+    },{
+      url:"/icons/browse-by/bb3.svg",
+      alt:"browse 3",
+    },{
+      url:"/icons/browse-by/bb4.svg",
+      alt:"browse 4",
+    }],
+    cutomerFeedback:[{
+        rate:5,
+        name:"Evon R.",
+        feedback:"I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations."
+    },{
+      rate:5,
+      name:"Sarah M.",
+      feedback:`"I'm blown away by the quality and style of the clothes I received from Shop.co. From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”`
+  },{
+    rate:5,
+    name:"Alex K.",
+    feedback:`"Finding clothes that align with my personal style used to be a challenge until I discovered Shop.co. The range of options they offer is truly remarkable, catering to a variety of tastes and occasions.”`
+  },{
+    rate:5,
+    name:"James L.",
+    feedback:`"As someone who's always on the lookout for unique fashion pieces, I'm thrilled to have stumbled upon Shop.co. The selection of clothes is not only diverse but also on-point with the latest trends.”`
+  },
+  ]
+  
+  };
+  
