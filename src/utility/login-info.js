81ebeb043863be58784251216a4a6d567f426205
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2024-12-03 12:03:03 +0530

    demo login array added

---
+++ src/utility/login-info.js
@@ -0,0 +1,9 @@
+// demo login user credentials
+export const userInfo = 
+[  
+    { id:1, name:"user1", email: "user1@gmail.com", password: "password123" },
+    { id:2, name:"user2", email: "user2@gmail.com", password: "securepass1" },
+    { id:3, name:"user3", email: "user3@gmail.com", password: "mypassword99" },
+    { id:4, name:"user4", email: "user4@gmail.com", password: "supersecret0" },
+    { id:5, name:"user5", email: "user5@gmail.com", password: "passphrase11" },
+]
