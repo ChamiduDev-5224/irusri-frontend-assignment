e33a35d856e941a73671d44042f63c6a1c826d14
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2024-12-03 11:58:52 +0530

    yup validation schema added

---
+++ src/utility/Validate.js
@@ -0,0 +1,21 @@
+import * as Yup from "yup";
+
+export const loginValidationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters long"),
+});
+
+export const singupValidationSchema = Yup.object().shape({
+  name: Yup.string()
+  .required("Name is required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters long"),
+});
