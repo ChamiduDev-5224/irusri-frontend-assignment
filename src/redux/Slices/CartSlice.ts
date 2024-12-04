94cf7c559e13a650730d48d54c148c2fe7c1a052
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2024-12-04 17:34:06 +0530

    Remove cart slice added

--- src/redux/Slices/CartSlice.ts
+++ src/redux/Slices/CartSlice.ts
@@ -12,15 +12,20 @@ export const cartSlice = createSlice({
       
       // Check if the item already exists in the cart
       const exist = state.cart.find((vl) => vl.id === action.payload.id && vl.color==action.payload.color && vl.size==action.payload.size);
-      
+       action.payload.cartId=state.cart[state.cart.length-1]?.cartId>0?state.cart[state.cart.length-1]?.cartId+1:0+1;
+
       if (!exist) {
         state.cart.push(action.payload);
       } else {
         exist.qty+=action.payload.qty
       }
     },
+    removeItem: (state, action) => {
+      const newstate = state.cart.filter((val) => val.cartId !== action.payload);
+      state.cart = newstate;
+    },
   },
 });
 
-export const { cartInfo } = cartSlice.actions;
+export const { cartInfo,removeItem } = cartSlice.actions;
 export default cartSlice.reducer;
