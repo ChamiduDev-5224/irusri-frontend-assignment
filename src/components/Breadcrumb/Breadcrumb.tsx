781d638bc82dffe924859f5ae9456f7e9c04d342
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2024-12-04 17:34:33 +0530

    breadcrumb minor issue fixed

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -5,11 +5,11 @@ export const Breadcrumb: React.FC<breadcrumbInterface> = ({ array }) => {
   let arrayLength: number = array.length;
   return (
     <div className="flxr gap-2">
-      {array?.map((val, index) => {
+      {array?.map((val,index) => {
         return (
           <div key={index}>
             <span className="cursor-pointer">{val}</span>
-            {index !== arrayLength - 1 && <span>˃</span>}
+            {index !== arrayLength - 1 && <span> ˃ </span>}
           </div>
         );
       })}
