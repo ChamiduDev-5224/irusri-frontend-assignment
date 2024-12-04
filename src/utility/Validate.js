5b7437daae94b1fb8306a6f4342d004e4548c9bf
ChamiduDev-5224 <pplchamiduravihara@gmail.com>
2024-12-04 17:33:44 +0530

    products validate function added

--- src/utility/Validate.js
+++ src/utility/Validate.js
@@ -10,8 +10,7 @@ export const loginValidationSchema = Yup.object().shape({
 });
 
 export const singupValidationSchema = Yup.object().shape({
-  name: Yup.string()
-  .required("Name is required"),
+  name: Yup.string().required("Name is required"),
   email: Yup.string()
     .email("Invalid email address")
     .required("Email is required"),
@@ -19,3 +18,17 @@ export const singupValidationSchema = Yup.object().shape({
     .required("Password is required")
     .min(8, "Password must be at least 8 characters long"),
 });
+
+// product propertise validate befor adding cart
+export const checkProduct = (color, size, qty) => {
+  if (color == 0) {
+    return { msg: "Please, select color you need.", sts: 0 };
+  }
+  if (qty == 0) {
+    return { msg: "Please, select qty you need.", sts: 0 };
+  }
+  if (size == 0) {
+    return { msg: "Please, select size you need.", sts: 0 };
+  }
+  return { msg: "", sts: 1 };
+};
